217cb5db3af024bed1542e20051ece1e8328a54f
Claire MacDonald <cemacdonald2@miners.utep.edu>
2024-10-27 09:40:57 -0600

    final version of uniform random

--- ur.py
+++ ur.py
@@ -12,12 +12,40 @@ def uniform_random(board, last):
 
     index = random.randint(0,len(legal_moves))
 
+    if len(legal_moves) == 0:
+        return None
+
     return legal_moves[index]
 
+
+def get_board_and_move(file):
+    board = []
+    with open(file, 'r') as f:
+        last= str(f.readline().strip)
+        i = 0
+        while i < 6:
+            row = list(f.readline().strip)
+            board.append(row)
+            i+=1
+    return board, last
+        
+
 def main(): 
+    if len(sys.argv) != 4:
+        print("Correct Terminal Input: python ur.py file_name.txt output_mode #_of_simulations")
+        sys.exit(1)
+    file = sys.argv[1]
+    output_mode = sys.argv[2]
+    num_simulations = int(sys.argv[3])
 
+    if output_mode not in ['None', 'Brief', 'Verbose']:
+        print("Error, output mode not in the list of accepted output mode")
+        sys.exit(1)
+    board, last = get_board_and_move(file)
 
-    return     
+    move = uniform_random(board, last)
+
+    print(f"FINAL Move selected: {move}")
 
 
 if __name__ == '__main__':
