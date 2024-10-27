da3f9c2d019ff98ad750a126815588867782797c
Claire MacDonald <cemacdonald2@miners.utep.edu>
2024-10-27 15:50:02 -0600

    renamed variables and added testing game

--- ur.py
+++ ur.py
@@ -1,19 +1,15 @@
 import random 
 import sys
-def uniform_random(board, last):
-    if last != 0:
-        return None
-
+def uniform_random(board, player):
     legal_moves = []
     for col in range(7):
         if board[0][col] == 'O':
             legal_moves.append(col + 1)
 
-
-    index = random.randint(0,len(legal_moves))
-
     if len(legal_moves) == 0:
         return None
+
+    index = random.randint(0,len(legal_moves))
 
     return legal_moves[index]
 
@@ -21,13 +17,16 @@ def uniform_random(board, last):
 def get_board_and_move(file):
     board = []
     with open(file, 'r') as f:
-        last= str(f.readline().strip)
+        f.readline()
+
+        player = f.readline().strip()
+
         i = 0
         while i < 6:
             row = list(f.readline().strip)
             board.append(row)
             i+=1
-    return board, last
+    return board, player
         
 
 def main(): 
@@ -41,9 +40,9 @@ def main():
     if output_mode not in ['None', 'Brief', 'Verbose']:
         print("Error, output mode not in the list of accepted output mode")
         sys.exit(1)
-    board, last = get_board_and_move(file)
+    board, player = get_board_and_move(file)
 
-    move = uniform_random(board, last)
+    move = uniform_random(board, player)
 
     print(f"FINAL Move selected: {move}")
 
