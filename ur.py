b497818b1f5510348501ac2863be1a539ca8a2e3
Claire MacDonald <cemacdonald2@miners.utep.edu>
2024-10-26 18:59:18 -0600

    base for Uniform Random

--- ur.py
+++ ur.py
@@ -1,11 +1,24 @@
-import numpy as np
+import random 
+import sys
+def uniform_random(board, last):
+    if last != 0:
+        return None
 
-def ur():
-    return 
+    legal_moves = []
+    for col in range(7):
+        if board[0][col] == 'O':
+            legal_moves.append(col + 1)
 
 
-def main():
-    return
+    index = random.randint(0,len(legal_moves))
+
+    return legal_moves[index]
+
+def main(): 
+
+
+    return     
+
 
 if __name__ == '__main__':
     main()
